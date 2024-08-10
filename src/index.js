2f619aad94075d5375b048d968c05807a2a0b72f
rodri720 <tomiyrami@gamil.com>
2024-08-10 11:45:15 -0300

    formulario consultas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import ReactDOM from "react-dom"
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
